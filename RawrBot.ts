10547d2971c48ccd65d87155744350ae09c5dec0
stripes!!! :3 <89811520+ArrowAced@users.noreply.github.com>
2024-12-01 18:09:27 -0500

    automatic reconnection

--- RawrBot.ts
+++ RawrBot.ts
@@ -18,14 +18,46 @@ export class RawrBot {
         this._username = username
         this._ws = new WebSocket(serverURL) 
         this._ws.addEventListener("message",(e) => {
-            this._ws?.send(JSON.stringify({command: "login_pswd", username: username, password: password}))
+            this._ws?.send(JSON.stringify({command: "login_pswd", username: username, password: password, listener: "auth"}))
         }, {once:true})
         this._ws.addEventListener("message",(e) => {
+            //console.log(JSON.parse(e.data))
             this._handlepacket(JSON.parse(e.data))
+        })
+        this._ws.addEventListener("close", () => {
+            this.relog();
+        })
+    }
+
+    private relog() {
+        console.log("disconnecting! attempting to reconnect...")
+        if (!this._token) {
+            console.error("no token to reconnect with!")
+            return
+        }
+        this._ws = new WebSocket(serverURL) 
+        this._ws.addEventListener("message",(e) => {
+            this._ws?.send(JSON.stringify({command: "login_token", username: this._username, token: this._token, listener: "token"}))
+        }, {once:true})
+        this._ws.addEventListener("message",(e) => {
+            //console.log(JSON.parse(e.data))
+            this._handlepacket(JSON.parse(e.data))
+        })
+        this._ws.addEventListener("close", () => {
+            this.relog();
         })
     }
     //todo: handle greet and token and stuff
     private _handlepacket(packet: any) {
+        if (Object.hasOwn(packet,"listener")) {
+            if (packet.listener == "auth") {
+                this._token = packet.token
+                console.log("logged in with password!")
+            }
+            if (packet.listener == "token") {
+                console.log("logged in with token!")
+            }
+        }
         if (Object.hasOwn(packet, "command")) {
             switch (packet.command) {
                 case("new_post"): this._posts.push(packet.data); this._events.post.forEach((f) => {f(packet.data)})
